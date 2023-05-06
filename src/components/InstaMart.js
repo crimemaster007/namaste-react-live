8377e6eb5d725a8658a7371b1ab816e8619e8445
crimemaster007 <prachand9211@gmail.com>
2023-05-06 23:37:27 +0530

    added collpasable accordion

--- src/components/InstaMart.js
+++ src/components/InstaMart.js
@@ -1,8 +1,56 @@
+import { useState } from "react";
+
+const Section = ({ title, description, isVisible, setIsVisble, setHide }) => {
+
+    return (
+        <div className="border border-black p-2 m-2">
+            <h3 className="font-bold text-xl">{title}</h3>
+            {isVisible ? (
+                <button
+                    onClick={() => setHide()}
+                    className="cursor-pointer underline"
+                >Hide</button>) : (
+                <button
+                    onClick={() => setIsVisble()}
+                    className="cursor-pointer underline"
+                >Show</button>)
+            }
+
+            {isVisible && <p>{description}</p>}
+        </div>
+    )
+}
+// lifting the state up
+
 const InstaMart = () => {
+    const [isVisibleSection, setIsVisibleSection] = useState("about");
     return (
         <div>
-            <h1>Instamart</h1>
-            <h1>100's of component inside this</h1>
+            <h1 className="text-3xl p-2 m-2 font-bold">Instamart</h1>
+            <Section
+                title={"About"}
+                description={"This is the about section of Instamart.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."}
+                isVisible={isVisibleSection === "about"}
+                setIsVisble={() => setIsVisibleSection("about")}
+                setHide={() => setIsVisibleSection("")}
+
+
+            />
+
+            <Section
+                title={"Team Instamart"}
+                description={"This is the team section of Instamart. The team has 50 members...Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."}
+                isVisible={isVisibleSection === "Team Instamart"}
+                setIsVisble={() => setIsVisibleSection("Team Instamart")}
+                setHide={() => setIsVisibleSection("")}
+            />
+            <Section
+                title={"Career"}
+                description={"This is the career section of Instamart. The team has 50 members & we are expanding...Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."}
+                isVisible={isVisibleSection === "Career"}
+                setIsVisble={() => setIsVisibleSection("Career")}
+                setHide={() => setIsVisibleSection("")}
+            />
         </div>
     );
 };
